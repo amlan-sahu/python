52f4a16c67f05d407eb2a46e5ce0289cb0532574
amlan-sahu <amlan9531@gmail.com>
2025-06-18 16:53:23 +0530

    Add weather-based activity suggestion script

---
+++ condi_prob/05_sol.py
@@ -0,0 +1,10 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a walk"
+elif weather == "Rainy":
+    activity = "Stay indoors"
+elif weather == "Snowy":
+    activity = "Build a snowman"
+
+print(f"The weather is {weather}. You should: {activity}")
