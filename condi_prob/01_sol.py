a3d35637cd7477e8a5d96de8df214a83a4d709c5
amlan-sahu <amlan9531@gmail.com>
2025-06-18 16:05:39 +0530

    Fix input prompt and formatting in age classification script

--- condi_prob/01_sol.py
+++ condi_prob/01_sol.py
@@ -1,11 +1,11 @@
-age = input("Privide me age \n23")
+age = input("Privide me age \n")
 int_age = int(age)
 
-if(int_age < 13):
+if int_age < 13:
     print("You are a child")
-elif(int_age < 20):
+elif int_age < 20:
     print("You are a teenager")
-elif(int_age < 59):
+elif int_age < 59:
     print("You are an adult")
 else:
     print("You are senior")
