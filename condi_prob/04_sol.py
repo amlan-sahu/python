3889100ce0a9093512a5424d0bf472f09e297663
amlan-sahu <amlan9531@gmail.com>
2025-06-18 16:51:01 +0530

    Add banana ripeness classification based on color

---
+++ condi_prob/04_sol.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("The banana is unripe.")
+    elif color == "Yellow":
+        print("The banana is ripe.")
+    elif color == "Brown":
+        print("The banana is overripe.")
